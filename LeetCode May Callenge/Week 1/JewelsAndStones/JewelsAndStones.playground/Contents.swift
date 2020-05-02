748bbe0bb08956c745b1f868c7dc40f7948ba004
esraa <esraa.moustafa.9@gmail.com>
2020-05-02 23:01:38 +0200

    solve Jewels and stones problem

---
+++ LeetCode May Callenge/Week 1/JewelsAndStones/JewelsAndStones.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class Solution {
+    func numJewelsInStones(_ J: String, _ S: String) -> Int {
+        var count = 0
+        S.forEach {
+            if J.contains($0) {
+                count += 1
+            }
+        }
+        return count
+    }
+}
